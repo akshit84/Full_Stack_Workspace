857c3d301dda420cf43bcc6ac27938d8ed160547
akshit84 <akshitbhadesiya84@gmail.com>
2025-11-18 14:56:53 +0530

    useState example and understand props

---
+++ React/react-props-tailwind/src/App.jsx
@@ -0,0 +1,27 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import BgColor from './components/BgColor'
+import Card from './components/Card'
+
+function App() {
+  // const [count, setCount] = useState(0)
+  // let myArr = [1, 2, 3]
+  const myObj = {
+    username: "Akshit"
+  }
+
+  return (
+    <>
+      <h1 className='bg-purple-500 text-black p-5 rounded-xl mb-5'>Tailwind Test</h1>
+      <Card heading="Physics" />
+      <Card heading="Science of Chemistry" />
+      <Card heading={myObj.username}/>
+
+      {/* <BgColor /> */}
+    </>
+  )
+}
+
+export default App
